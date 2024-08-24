a749faea0ce2d3c3f378a2539239890001fe3f26
WTY2002 <2012180708@qq.com>
2024-08-24 15:25:30 +0800

    commit：更新脚本和测试文件

---
+++ wty/reverse_similarity_query_matrix/script/generate_data.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <cstring>
+#include <mutex>
+using namespace std;
+
+
+void createData() {
+    // 定义生成数据文件的路径
+    const char* fileString1 = "C:\\Users\\uu\\Desktop\\privacy_tool_test\\wty\\reverse_similarity_query_matrix\\data1.txt";
+    ofstream outfile(fileString1);
+
+    if (!outfile.is_open()) {
+        cerr << "Error opening file for writing: " << fileString1 << endl;
+        return;
+    } else {
+        cout << "File opened successfully: " << fileString1 << endl;
+    }
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 1000);
+
+    for (int i = 0; i < 10000; ++i) { // 写入数据
+        for (int j = 0; j < 4; ++j) {
+            if (j != 0) {
+                outfile << " ";
+            }
+            outfile << dis(gen);
+        }
+        outfile << "\n";
+    }
+
+    
+
+    outfile.close();
+    cout << "Data written successfully to: " << fileString1 << endl;
+}
+
+void createData2() {
+    // 定义生成数据文件的路径
+    const char* fileString1 = "C:\\Users\\uu\\Desktop\\privacy_tool_test\\wty\\reverse_similarity_query_matrix\\data2.txt";
+    ofstream outfile(fileString1);
+
+    if (!outfile.is_open()) {
+        cerr << "Error opening file for writing: " << fileString1 << endl;
+        return;
+    } else {
+        cout << "File opened successfully: " << fileString1 << endl;
+    }
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 1000);
+
+    for (int i = 0; i < 1000; ++i) { // 写入数据
+        for (int j = 0; j < 4; ++j) {
+            if (j != 0) {
+                outfile << " ";
+            }
+            outfile << dis(gen);
+        }
+        outfile << "\n";
+    }
+
+    
+
+    outfile.close();
+    cout << "Data written successfully to: " << fileString1 << endl;
+}
+
+int main() {
+    createData();
+    createData2();
+    return 0;
+}
---
+++ wty/skyline_matrix/script/generate_data.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <cstring>
+#include <mutex>
+using namespace std;
+
+
+void createData() {
+    // 定义生成数据文件的路径
+    const char* fileString1 = "C:\\Users\\uu\\Desktop\\privacy_tool_test\\wty\\skyline_matrix\\data.txt";
+    ofstream outfile(fileString1);
+
+    if (!outfile.is_open()) {
+        cerr << "Error opening file for writing: " << fileString1 << endl;
+        return;
+    } else {
+        cout << "File opened successfully: " << fileString1 << endl;
+    }
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 1000);
+
+    for (int i = 0; i < 1000000; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            if (j != 0) {
+                outfile << " ";
+            }
+            outfile << dis(gen);
+        }
+        outfile << "\n";
+    }    
+
+    outfile.close();
+    cout << "Data written successfully to: " << fileString1 << endl;
+}
+
+int main() {
+    createData();
+    return 0;
+}
